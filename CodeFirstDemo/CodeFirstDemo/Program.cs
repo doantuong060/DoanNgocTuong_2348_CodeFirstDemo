aac012f13ee247880c754c614d056570530766ce
doantuong060 <doantuong060@gmail.com>
2026-04-10 23:21:32 +0700

    Step 6: Register DbContext

--- CodeFirstDemo/CodeFirstDemo/Program.cs
+++ CodeFirstDemo/CodeFirstDemo/Program.cs
@@ -1,10 +1,14 @@
+using CodeFirstDemo.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
